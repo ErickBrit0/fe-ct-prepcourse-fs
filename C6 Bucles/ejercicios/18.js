882b0e124e02b77d2e68c9fcdc358db36dcf2ca6
Erick Brito <javerick95@gmail.com>
2024-09-25 22:02:44 -0500

    Finish job
    avanze de ejercicios C6 js

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,15 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  let product = 1;
+  
+  const inicio = Math.min(a, b);
+  const fin = Math.max(a, b);
+
+  for(let i = inicio; i <= fin; i++) {
+    product *= i;
+  }
+  return product;
 }
 
 module.exports = productoEntreNúmeros;
